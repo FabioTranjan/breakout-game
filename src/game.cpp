b2be1ec5010b6ce3a9b36471e79aae83fab517e8
Fabio Tranjan <fabiotkx@gmail.com>
2018-09-09 21:23:48 -0300

    Rendering the image on the Game class.

--- src/game.cpp
+++ src/game.cpp
@@ -17,7 +17,16 @@ Game::~Game()
 
 void Game::Init()
 {
+  ResourceManager::LoadShader("shaders/sprite.vs", "shaders/sprite.fs", nullptr, "sprite");
 
+  glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(this->Width),
+    static_cast<GLfloat>(this->Height), 0.0f, -1.0f, 1.0f);
+  ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+  ResourceManager::GetShader("sprite").SetMatrix4f("projection", projection);
+
+  Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+
+  ResourceManager::LoadTexture("resources/awesomeface.png", GL_TRUE, "face");
 }
 
 void Game::Update(GLfloat dt)
@@ -32,5 +41,6 @@ void Game::ProcessInput(GLfloat dt)
 
 void Game::Render()
 {
-
+  Renderer->DrawSprite(ResourceManager::GetTexture("face"),
+    glm::vec2(200, 200), glm::vec2(300, 400), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
--- src/sprite_renderer.hpp
+++ src/sprite_renderer.hpp
@@ -5,8 +5,8 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include "texture.h"
-#include "shader.h"
+#include "texture.hpp"
+#include "shader.hpp"
 
 class SpriteRenderer
 {
